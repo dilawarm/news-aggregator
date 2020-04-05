e1f6adecd69117a1c7fdbc0fb3f710dea0d81ed3
Dilawar Mahmood <dilawarm@stud.ntnu.no>
2020-04-05 03:28:49 +0200

    iterating over sitemap

--- server.go
+++ server.go
@@ -23,9 +23,10 @@ func main() {
 	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemaps/index.xml")
 	bytes, _ := ioutil.ReadAll(resp.Body)
 	resp.Body.Close()
-
 	var s SitemapIndex
 	xml.Unmarshal(bytes, &s)
 
-	fmt.Println(s.Locations)
+	for _, Location := range s.Locations {
+		fmt.Printf("\n%s", Location)
+	}
 }
